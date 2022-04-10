4d23a2c6466c67457a61bdbcb7ecdaf162a7a86a
mondayyy <monday091233@gmail.com>
2022-04-09 23:13:48 -0400

    1

---
+++ MazeExplorer/mazeexplorer/test_maze_A.py
@@ -0,0 +1,13 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License
+
+from __future__ import print_function
+
+import os
+import random
+from maze_gene_A import generate_maze_A, WallNode
+
+import numpy as np
+
+test_maze = generate_maze_A()
+print(test_maze)
